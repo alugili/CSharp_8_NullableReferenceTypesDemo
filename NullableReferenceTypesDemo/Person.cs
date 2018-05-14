9814e13b01d1ccdcf775015642f6a8f88b63cf11
Bassam Alugili <balugili@hotmail.com>
2018-05-14 23:09:23 +0200

    Add files via upload
    example code!

---
+++ NullableReferenceTypesDemo/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace NullableReferenceTypesDemo
+{
+  class Person
+  {
+    public string Name { get; set; }
+    public string? NullableName { get; set; }
+
+    //public Person(string name)
+    //{
+    //  Name = name;
+    //}
+  }
+}
---
+++ NullableReferenceTypesDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace NullableReferenceTypesDemo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Tuple fix 7.3
+      var areEqual = (10, "abc") == (10, "abc"); // False in the old version!
+      areEqual.ToString();
+
+      string? nullableString = "Fadi"; // Fadi to null will
+      Console.WriteLine(nullableString.Length);     // WARNING: may be null
+
+      if (nullableString != null)
+      {
+        Console.WriteLine(nullableString.Length);   // Ok, not null here 
+      }
+
+      if (nullableString == null)
+      {
+        Console.WriteLine(nullableString?.Length);     // No WARNING: ? Opreator
+      }
+
+      Person person = new Person();
+
+      // Null Exception old style no warning!
+      // var name = person.Name.ToString();
+
+      string? userName = null;
+      person.Name = userName;
+
+      // This line will removes the warning! the compiler ist smart!
+      // person.NullableName = "Rami";
+
+      // Null Exception with null warning!
+      var name = person.NullableName.ToString();
+
+      // Error suppress
+      //var nameWithErrorSuppress = person.NullableName!.ToString();
+
+      Person secondPerson = null;
+      // Not possible to use a null
+      // secondPerson.Name;
+
+      Person? personNullable = null;
+
+      // Possible to use 
+      var nullFromNullable = personNullable?.ToString();
+    }
+  }
+}
